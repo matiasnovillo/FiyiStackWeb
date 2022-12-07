00c4983ad10469da76332f43f5afc4755e89fc4c
Matias Novillo <novillo.matias1@gmail.com>
2022-12-07 17:09:58 -0300

    Se subio el repositorio de forma remota

---
+++ FiyiStackWeb/wwwroot/js/Basic.Core/Failure/jQuery/FailureNonQuery_jQuery.js
@@ -0,0 +1,109 @@
+
+
+/*
+ * GUID:e6c09dfe-3a3e-461b-b3f9-734aee05fc7b
+ * Licensed to a unique person with this Token:IAmTheOwnerOfThis
+ * 
+ * Coded by www.fiyistack.com
+ * Copyright © 2021
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * Auto generated code. Add your custom code after the last line of auto generation
+*/
+
+//Stack: 10
+
+//Last modification on: 07/12/2022 17:01:54
+
+$(document).ready(function () {
+    //This fix an style error in Quill
+    $("span.ql-picker-label svg[viewBox]").css("width", "20px");
+});
+
+//Used for file input
+
+
+//Create a formdata object
+var formData = new FormData();
+$("#basic.core-failure-insert-or-update-button").on("click", function (e) {
+    //Stop stuff happening
+    e.stopPropagation();
+    e.preventDefault();
+
+    //Add or edit value
+    formData.append("basic.core-failure-title-page", $("#basic.core-failure-title-page").html());
+    formData.append("basic.core-failure-failureid-input", $("#basic.core-failure-failureid-input").val());
+
+    formData.append("basic.core-failure-httpcode-input", $("#basic.core-failure-httpcode-input").val());
+    formData.append("basic.core-failure-emergencylevel-input", $("#basic.core-failure-emergencylevel-input").val());
+    formData.append("basic.core-failure-message-input", $("#basic.core-failure-message-input").val());
+    formData.append("basic.core-failure-stacktrace-input", $("#basic.core-failure-stacktrace-input").val());
+    formData.append("basic.core-failure-source-input", $("#basic.core-failure-source-input").val());
+    formData.append("basic.core-failure-comment-input", $("#basic.core-failure-comment-input").val());
+    formData.append("basic.core-failure-usercreationid-input", $("#basic.core-failure-usercreationid-input").val());
+    formData.append("basic.core-failure-userlastmodificationid-input", $("#basic.core-failure-userlastmodificationid-input").val());
+    
+
+    //Setup request
+    var xmlHttpRequest = new XMLHttpRequest();
+    //Set event listeners
+    xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
+        //Show success button and success message modal
+        $("#basic.core-failure-insert-or-update-message").addClass("btn-secondary");
+        $("#basic.core-failure-insert-or-update-message").removeClass("btn-success");
+        $("#basic.core-failure-insert-or-update-message").removeClass("btn-error");
+        $("#basic.core-failure-insert-or-update-message").removeAttr("data-toggle");
+        $("#basic.core-failure-insert-or-update-message").removeAttr("data-target");
+        $("#basic.core-failure-insert-or-update-message").html(`Sending data. Please, wait`);
+    });
+    xmlHttpRequest.upload.addEventListener("progress", function (e) {
+        // While sending and loading data.
+    });
+    xmlHttpRequest.upload.addEventListener("load", function (e) {
+        // When the request has successfully completed.
+    });
+    xmlHttpRequest.upload.addEventListener("loadend", function (e) {
+        // When the request has completed (either in success or failure).
+    });
+    xmlHttpRequest.upload.addEventListener("error", function (e) {
+        // When the request has failed.
+    });
+    xmlHttpRequest.upload.addEventListener("abort", function (e) {
+        // When the request has been aborted. 
+    });
+    xmlHttpRequest.upload.addEventListener("timeout", function (e) {
+        // When the author specified timeout has passed before the request could complete
+    });
+    xmlHttpRequest.onload = function () {
+        console.log(xmlHttpRequest);
+        if (xmlHttpRequest.status >= 400) {
+            //Show error button and error message modal
+            $("#basic.core-failure-insert-or-update-message").addClass("btn-danger");
+            $("#basic.core-failure-insert-or-update-message").removeClass("btn-success");
+            $("#basic.core-failure-insert-or-update-message").removeClass("btn-secondary");
+            $("#basic.core-failure-insert-or-update-message").attr("data-toggle", "modal");
+            $("#basic.core-failure-insert-or-update-message").attr("data-target", "#basic.core-failure-error-message-modal");
+            $("#basic.core-failure-insert-or-update-message").html(`<i class="fas fa-exclamation-triangle"></i> 
+                                                                There was an error while sending the data`);
+            $("#basic.core-failure-error-message-title").html("There was an error while sending the data");
+            $("#basic.core-failure-error-message-text").html(xmlHttpRequest.response);
+            console.log("Error:" + xmlHttpRequest.response);
+        }
+        else {
+            //Show success button
+            $("#basic.core-failure-insert-or-update-message").addClass("btn-success");
+            $("#basic.core-failure-insert-or-update-message").removeClass("btn-error");
+            $("#basic.core-failure-insert-or-update-message").removeClass("btn-secondary");
+            $("#basic.core-failure-insert-or-update-message").removeAttr("data-toggle");
+            $("#basic.core-failure-insert-or-update-message").removeAttr("data-target");
+            $("#basic.core-failure-insert-or-update-message").html(`<i class="fas fa-check"></i>
+                                                                Data sent successfully`);
+        }
+    };
+    //Open connection
+    xmlHttpRequest.open("POST", "/api/Testing/Test/1/InsertOrUpdateAsync", true);
+    //Send request
+    xmlHttpRequest.send(formData);
+});
