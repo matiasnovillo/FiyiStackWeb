48fb8754e02d11c56795dfaf94dbe080dc46df40
Matias Novillo <novillo.matias1@gmail.com>
2022-12-16 02:23:16 -0300

    Se hizo la pagina Pricing y se agregaron las areas FiyiStack y FiyiSchool

---
+++ FiyiStackWeb/Areas/FiyiStack/Pages/Pricing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FiyiStackWeb.Areas.FiyiStack
+{
+    public class PricingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FiyiStackWeb/Areas/FiyiStack/Products/Pages/FiyiRequirements.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FiyiStackWeb.Areas.FiyiStack.Products.Pages
+{
+    public class FiyiRequirementsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
