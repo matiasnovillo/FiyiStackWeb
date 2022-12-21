cbb9d988d70ca504ed72ad1570ca75b48fdf8534
Matias Novillo <novillo.matias1@gmail.com>
2022-12-21 07:48:23 -0300

    Se hicieron mejoras en Index y _Layout

--- FiyiStackWeb/Pages/Index.cshtml.cs
+++ FiyiStackWeb/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -19,7 +20,33 @@ namespace FiyiStackWeb.Pages
 
         public void OnGet()
         {
+            //Get UserId from Session
+            int UserId = HttpContext.Session.GetInt32("UserId") ?? 0;
 
+            if (UserId == 0)
+            {
+                //User not found
+                ViewData["EnterButton"] = $@"<li class='nav-item'>
+                                                <a href='/CMSCore/Login' class='btn btn-white mt-1 ml-2'>
+                                                    <i class='fas fa-user'></i> 
+                                                    <span class='nav-link-inner--text'>
+                                                        Login
+                                                    </span>
+                                                </a>
+                                            </li>";
+            }
+            else
+            {
+                //User found
+                ViewData["EnterButton"] = $@"<li class='nav-item'>
+                                                <a href='/CMSCore/DashboardIndex' class='btn btn-white mt-1 ml-2'>
+                                                    <i class='fas fa-user'></i> 
+                                                    <span class='nav-link-inner--text'>
+                                                        Enter dashboard
+                                                    </span>
+                                                </a>
+                                            </li>";
+            }
         }
     }
 }
