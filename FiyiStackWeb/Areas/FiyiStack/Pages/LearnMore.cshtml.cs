bccf4046c6c20a27bf898c36f9249312a09f7940
Matias Novillo <novillo.matias1@gmail.com>
2022-12-22 14:33:14 -0300

    Se actualizo el backup de la BD

---
+++ FiyiStackWeb/Areas/FiyiStack/Pages/LearnMore.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+
+namespace FiyiStackWeb.Areas.FiyiStack.Pages
+{
+    public class LearnMoreModel : PageModel
+    {
+        public void OnGet()
+        {
+            //Get UserId from Session
+            int UserId = HttpContext.Session.GetInt32("UserId") ?? 0;
+
+            if (UserId == 0)
+            {
+                //User not found
+                ViewData["EnterButton"] = $@"<li class='nav-item'>
+                                                <a href='/CMSCore/Login' class='btn btn-white mt-1 ml-2'>
+                                                    <i class='fas fa-user'></i> 
+                                                    <span class='nav-link-inner--text'>
+                                                        Login
+                                                    </span>
+                                                </a>
+                                            </li>";
+            }
+            else
+            {
+                //User found
+                ViewData["EnterButton"] = $@"<li class='nav-item'>
+                                                <a href='/CMSCore/DashboardIndex' class='btn btn-white mt-1 ml-2'>
+                                                    <i class='fas fa-user'></i> 
+                                                    <span class='nav-link-inner--text'>
+                                                        Enter dashboard
+                                                    </span>
+                                                </a>
+                                            </li>";
+            }
+        }
+    }
+}
