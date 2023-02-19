374d6e20493f347ee90c19e9ad9851a961cf5751
Matias Novillo <novillo.matias1@gmail.com>
2023-02-19 09:50:52 -0300

    Se mejoro el ContactMe

--- FiyiStackWeb/wwwroot/js/FiyiStack/ContactMe.js
+++ FiyiStackWeb/wwwroot/js/FiyiStack/ContactMe.js
@@ -1,78 +1,66 @@
 ﻿$(document).ready(function () {
-    $("#message").hide();
-});
 
-//Create a formdata object
-var formData = new FormData();
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName("needs-validation");
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener("submit", function (event) {
 
-$("#send-message").on("click", function (e) {
-    formData.append("name", $("#name").val());
-    formData.append("surname", $("#surname").val());
-    formData.append("email", $("#email").val());
-    formData.append("textarea-message", $("#textarea-message").val());
+            event.preventDefault();
+            event.stopPropagation();
 
-    $("#message").show();
+            if (form.checkValidity() === true) {
+                //Create a formdata object
+                var formData = new FormData();
 
-    //Setup request
-    var xmlHttpRequest = new XMLHttpRequest();
-    //Set event listeners
-    xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
-        $("#message").addClass("btn-white");
-        $("#message").removeClass("btn-danger");
-        $("#message").removeClass("btn-success");
-        $("#message").html(`Sending email. Please, wait...`);
-    });
-    xmlHttpRequest.upload.addEventListener("progress", function (e) {
-        // While sending and loading data.
-    });
-    xmlHttpRequest.upload.addEventListener("load", function (e) {
-        // When the request has successfully completed.
-    });
-    xmlHttpRequest.upload.addEventListener("loadend", function (e) {
-        // When the request has completed (either in success or failure).
-    });
-    xmlHttpRequest.upload.addEventListener("error", function (e) {
-        // When the request has failed.
-    });
-    xmlHttpRequest.upload.addEventListener("abort", function (e) {
-        // When the request has been aborted. 
-    });
-    xmlHttpRequest.upload.addEventListener("timeout", function (e) {
-        // When the author specified timeout has passed before the request could complete
-    });
-    xmlHttpRequest.onload = function () {
-        console.log(xmlHttpRequest);
-        if (xmlHttpRequest.status >= 400) {
-            //Show danger button
-            $("#message").addClass("btn-danger");
-            $("#message").removeClass("btn-white");
-            $("#message").removeClass("btn-success");
-            $("#message").html(`<i class="fas fa-exclamation-triangle"></i> 
-                                    There was an error while trying to send message. Try again.`);
-            console.log("Error:" + xmlHttpRequest.response);
-        }
-        else {
-            if (xmlHttpRequest.response == "Message sent successfully") {
-                //Show success button
-                $("#message").addClass("btn-success");
-                $("#message").removeClass("btn-white");
-                $("#message").removeClass("btn-danger");
-                $("#message").html(`<i class="fas fa-check"></i>
-                                    Message sent successfully`);
+                formData.append("name", $("#name").val());
+                formData.append("surname", $("#surname").val());
+                formData.append("email", $("#email").val());
+                formData.append("textarea-message", $("#textarea-message").val());
+
+                $("#message").show();
+
+                //Setup request
+                var xmlHttpRequest = new XMLHttpRequest();
+                //Set event listeners
+                xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
+                    $.notify({ message: "Sending email. Please, wait..." }, { type: "info", placement: { from: "bottom", align: "center" } });
+                });
+                xmlHttpRequest.onload = function () {
+                    console.log(xmlHttpRequest);
+                    if (xmlHttpRequest.status >= 400) {
+                        //ERROR
+                        $.notify({ message: "There was an error while trying to send message. Try again." }, { type: "danger", placement: { from: "bottom", align: "center" } });
+                        console.log(xmlHttpRequest);
+                    }
+                    else {
+                        if (xmlHttpRequest.response == "Message sent successfully") {
+                            //SUCCESS
+                            $.notify({ message: "Message sent successfully" }, { type: "success", placement: { from: "bottom", align: "center" } });
+                            $("#name").val("");
+                            $("#surname").val("");
+                            $("#email").val("");
+                            $("#textarea-message").val("");
+                        }
+                        else {
+                            //ERROR
+                            $.notify({ message: "The message could not be sent. Try again." }, { type: "danger", placement: { from: "bottom", align: "center" } });
+                            console.log(xmlHttpRequest);
+                        }
+                    }
+                };
+                //Open connection
+                xmlHttpRequest.open("POST", "/api/FiyiStack/1/ContactMe", true);
+                //Send request
+                xmlHttpRequest.send(formData);
             }
             else {
-                //Show danger button
-                $("#message").addClass("btn-danger");
-                $("#message").removeClass("btn-white");
-                $("#message").removeClass("btn-success");
-                $("#message").html(`<i class="fas fa-exclamation-triangle"></i> 
-                                    The message could not be sent. Try again.`);
-                console.log("Error:" + xmlHttpRequest.response);
+                $.notify({ message: "Please, complete all fields." }, { type: "warning", placement: { from: "bottom", align: "center" } });
             }
-        }
-    };
-    //Open connection
-    xmlHttpRequest.open("POST", "/api/FiyiStack/1/ContactMe", true);
-    //Send request
-    xmlHttpRequest.send(formData);
+
+
+            form.classList.add("was-validated");
+        }, false);
+    });
+
 });
